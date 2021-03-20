9fbff8beb94a3694d4d09da0f1606d3c1df315c0
cassiorsfreitas <cassio.rsfreitas@gmail.com>
2021-03-20 21:29:10 +0000

    added registerServiceWorker file and started grid css

--- app/src/index.js
+++ app/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './main/App';
+import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+registerServiceWorker();
--- app/src/main/App.jsx
+++ app/src/main/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-anonymous-default-export */
 import './App.css'
 import React from 'react'
 
